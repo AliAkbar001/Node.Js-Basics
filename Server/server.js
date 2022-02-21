362062e7ce0b21e06030c8d75243260bdce636a9
AliAkbar001 <786aliakbar.aa@gmail.com>
2022-02-21 19:06:57 +0500

    Response-HTML-File

--- Server/server.js
+++ Server/server.js
@@ -1,20 +1,37 @@
 const http = require('http')
 const hostname = 'localhost'
 const port = 3000
+const fs = require('fs')
 
 const server = http.createServer((req, res)=>{
     console.log('Request', req);
     console.log('Request URL', req.url);
     console.log('Request Method', req.method);
+
     //Plain Text Response
     // res.setHeader('Content-Type', 'text/plain')
     // res.write('Hello World!!!')
     // res.end()
+
     //Response in HTML
+    // res.setHeader('Content-Type', 'text/html')
+    // res.write('<h3>Hello World!!!</h3>')
+    // res.write('<p>This is Paragraph</p>')
+    // res.end()
+
+    //Response in HTML File
     res.setHeader('Content-Type', 'text/html')
-    res.write('<h3>Hello World!!!</h3>')
-    res.write('<p>This is Paragraph</p>')
-    res.end()
+    fs.readFile('index.html',(err,data)=>{
+        if(err){
+            console.log(err)
+            res.end()
+        }else{
+            // res.write(data)
+            // res.end()
+            //OR
+            res.end(data)
+        }
+    })
 });
 
 server.listen(port, hostname, () => {
