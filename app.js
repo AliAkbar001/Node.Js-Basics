8b207bc0f4062ada69654656b8c7ff99efc4eb3c
AliAkbar001 <786aliakbar.aa@gmail.com>
2022-02-21 12:56:12 +0500

    Modules

---
+++ app.js
@@ -0,0 +1,7 @@
+// const user = require('./user')
+// console.log(user)
+// console.log(user.user)
+// console.log(user.number)
+const {user,number} = require('./user')
+console.log(user)
+console.log(number)
---
+++ user.js
@@ -0,0 +1,17 @@
+const user = [{
+    name: "Ali",
+    age:24
+},{
+    name: "Ahmed",
+    age:23,
+    city:"Gujrat"
+},{
+    name: "Saad",
+    city:["Mandi","Gujrat"],
+    father:{
+        name:"Ali",
+        city:"Gujrat"
+    }
+}]
+const number = [1,2,3,4,5]
+module.exports = {user, number};
