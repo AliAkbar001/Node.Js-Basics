2755ea56144bd82bef7380579560ff4812bea24d
AliAkbar001 <786aliakbar.aa@gmail.com>
2022-02-21 18:46:09 +0500

    URL-REQ

--- Server/server.js
+++ Server/server.js
@@ -4,6 +4,7 @@ const port = 3000
 
 const server = http.createServer((req, res)=>{
     console.log('Request URL', req.url);
+    console.log('Request Method', req.method);
 });
 
 server.listen(port, hostname, () => {
