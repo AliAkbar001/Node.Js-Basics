425f49d52a071e4541ee323c7ae1ce1153523c51
AliAkbar001 <786aliakbar.aa@gmail.com>
2022-02-21 18:56:55 +0500

    Server-Response

--- Server/server.js
+++ Server/server.js
@@ -3,8 +3,18 @@ const hostname = 'localhost'
 const port = 3000
 
 const server = http.createServer((req, res)=>{
+    console.log('Request', req);
     console.log('Request URL', req.url);
     console.log('Request Method', req.method);
+    //Plain Text Response
+    // res.setHeader('Content-Type', 'text/plain')
+    // res.write('Hello World!!!')
+    // res.end()
+    //Response in HTML
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<h3>Hello World!!!</h3>')
+    res.write('<p>This is Paragraph</p>')
+    res.end()
 });
 
 server.listen(port, hostname, () => {
